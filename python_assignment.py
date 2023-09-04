5e0d6e73e688bd239111048ccdc3aaf16d57de08
VedanshGure <gureonlywork@gmail.com>
2023-09-04 12:14:13 +0530

    msg_cmt_03

--- python_assignment.py
+++ python_assignment.py
@@ -1,5 +1,51 @@
-# your python assignemnt
- 
+class Employee:
+    def __init__(self, emp_id, name, age, salary):
+        self.emp_id = emp_id
+        self.name = name
+        self.age = age
+        self.salary = salary
+
+    def __str__(self):
+        return f"{self.emp_id} {self.name} {self.age} {self.salary}"
 
 
-#.........
+def sort_employees(employees, key):
+    if key == 1:
+        return sorted(employees, key=lambda emp: emp.age)
+    elif key == 2:
+        return sorted(employees, key=lambda emp: emp.name)
+    elif key == 3:
+        return sorted(employees, key=lambda emp: emp.salary)
+    else:
+        print("Invalid sorting parameter")
+        return employees
+
+
+def print_employee_data(employees):
+    for emp in employees:
+        print(emp)
+
+
+if __name__ == "__main__":
+    employees = [
+        Employee("161E90", "Raman", 41, 56000),
+        Employee("161F91", "Himadri", 38, 67500),
+        Employee("161F99", "Jaya", 51, 82100),
+        Employee("171E20", "Tejas", 30, 55000),
+        Employee("171G30", "Ajay", 45, 44000),
+    ]
+
+    while True:
+        print("Sorting Options:")
+        print("1. Age")
+        print("2. Name")
+        print("3. Salary")
+        print("4. Quit")
+
+        choice = int(input("Enter your choice: "))
+
+        if choice == 4:
+            break
+
+        employees = sort_employees(employees, choice)
+        print_employee_data(employees)
